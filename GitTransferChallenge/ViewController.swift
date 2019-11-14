4885cefd44abcc92ea86fe1209206744f78e3b74
joshua <joshuanesbitt1234567@icloud.com>
2019-11-14 09:10:34 -0600

    Finished coding

--- GitTransferChallenge/ViewController.swift
+++ GitTransferChallenge/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var Crate: UIImageView!
 
+    @IBAction func imageAppear(_ sender: Any) {
+        if Crate.isHidden == false {
+            Crate.isHidden = true
+        } else {
+            Crate.isHidden = false
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
